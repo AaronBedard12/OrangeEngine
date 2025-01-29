c1cfc31f2e75a1a0beab86671d6c18e40a8657ab
“Aaron <sirorangepeel@gmail.com>
2025-01-28 23:52:49 -0700

    Added Default values for the name and path space

---
+++ EngineEditor/GameProject/CreateProject.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrangeEditor.GameProject
+{
+    class CreateProject : ViewModelBase
+    {
+        private string _name = "New Project";
+        public string Name 
+        {
+            get => _name;  
+            set 
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged(nameof(Name));
+                }
+            } 
+        }
+
+        private string _path = $@"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\OrangeProjects\";
+        public string Path
+        {
+            get => _path;
+            set
+            {
+                if (_path != value)
+                {
+                    _path = value;
+                    OnPropertyChanged(nameof(Path));
+                }
+            }
+        }
+
+    }
+}
